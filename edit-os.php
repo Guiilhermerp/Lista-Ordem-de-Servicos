2702157add060c4487900d783a49cac5d87dcb1c
Guiilhermerp <guiigrodrigues@gmail.com>
2019-11-27 17:41:37 -0300

    Edit pronto e erro no front(show-os)

--- edit-os.php
+++ edit-os.php
@@ -1,21 +1,45 @@
+<?php
+
+    // Verificando se o formulario está sendo enviado
+    if($_POST){
+        // Validar informações
+
+        // Preparar consulta de atualização na base
+        $dbc->prepare("UPDATE 
+                        oss 
+                        SET 
+                            endereco=:endereco, 
+                            id_bairro:id_bairro,
+                            id_tipo:id_tipo
+                            
+                        WHERE id=:id");
+        // Executar consulta
+
+        // Redirecionar para a lista de OSs (index.php)
+        header('Location: index.php');
+    }
+
+?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+
+    <title></title>
 </head>
 
 <body>
     <?php 
-        include "./includes/dbc.php";
+include "./includes/dbc.php";
 
-        $id = $_GET['$id'];
+$id = $_GET['id'];
 
-        // Preparando a consulta
-        $query = $dbc->prepare("SELECT
+$query = $dbc->prepare("SELECT
                         o.*,
                             b.nome,
                             t.nome
@@ -23,19 +47,108 @@
                         oss o
                             INNER JOIN bairros b ON o.id_bairro=b.id
                             INNER JOIN tipos_de_os t ON o.id_tipo=t.id
-                        )
-                        WHERE o.id=:id;");
+                        ) WHERE o.id=:id;");
 
-        // Executando
-        $query->execute([':id' => $id]);
+                        //Executando a consulta preparada
+                        $query->execute([
+                            ':id' => $id]);
 
-        // Recuperando os dados
-        $os = $query->fetchAll(PDO::FETCH_ASSOC);
-    ?>
+                        //Capturando o resultado da minha consulta [só a primeira linha]    
+                        $os = $query->fetchAll(PDO::FETCH_ASSOC)[0];
+                        
 
-    <form action="" method="post">
-        <input type="text" name="" id="" value="<?=$os[0]['endereco']?>">
-    </form>
+
+// Preparar a consulta para levantar os bairros
+$query = $dbc->prepare("SELECT id, nome FROM bairros");
+
+// Executando a consulta
+$query->execute();
+
+// Capturar o resultado da consulta
+$bairros = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
+// Preparar a consulta para levantar os tipos
+$query = $dbc->prepare("SELECT id, nome FROM tipos_de_os");
+
+// Executar a consulta
+$query->execute();
+
+// Capturar o resultado da minha consulta
+$tipos = $query->fetchAll(PDO::FETCH_ASSOC);
+
+// echo("<pre>");
+// var_dump($bairros);
+// echo("</pre>");
+// exit;
+
+  ?>
+    <main class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <form action="" method="post">
+
+                    <!-- NOME DA RUA -->
+                    <div class="form-group">
+                    <label for="endereco">Endereço</label>
+                    <input type="text" class="form-control" name="" id="endereco" aria-describedby="endereco" value="<?=$os['endereco']?>">
+                    </div>
+                    
+                    <!-- BAIRRO -->
+                    <div class="form-group">
+                    <label for="bairro">Bairro</label>
+                    <select class="form-control" name="" id="bairro" >
+                        <option disable>Selecione um bairro</option>
+                        <!-- Loop para pegar todos os bairros // Condição IF para selecionar o BAIRRO do ENDERECO -->
+                        <?php foreach ($bairros as $bairro): ?>
+                            <option <?= $bairro['id'] == $os['id_bairro'] ? 'selected' : '' ?> value="<?=$bairro['id']?>"><?=$bairro['nome']?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    </div>
+
+                    <!-- TIPOS DE OS -->
+                    <div class="form-group">
+                        <label for="tipos">Tipos</label>
+                        <select class="form-control">
+                        <option disabled>Selecione um tipo</option>
+                            <?php foreach ($tipos as $t): ?>
+                        <option
+                            <?=$t['id'] == $os['id_tipo'] ? 'selected' : ''?>
+                            value="<?=$t['id']?>">
+                            <?=$t['nome']?>
+                        </option>
+                            <?php endforeach;?>
+                        </select>
+                    </div>
+
+                    <!-- Duracao Prevista -->
+                    <div class="form-group">
+                        <label for="duracao_prev">Duração Prevista</label>
+                        <input type="number" class="form-control" name="" id="duracao_prev" aria-describedby="helpId" value="<?=$os['duracao_prev']?>"> 
+                    </div>
+                    <!-- Duracao Real -->
+                    <div class="form-group">
+                        <label for="duracao_real">Duração Real</label>
+                        <input type="number" class="form-control" name="" id="duracao_real" aria-describedby="helpId" value="<?=$os['duracao_real']?>">
+                    </div>
+                    
+                    <!-- VALOR -->
+                    <div class="form-group">
+                        <label for="valor">Valor</label>
+                        <input type="number" class="form-control" name="" id="valor" aria-describedby="helpId" value="<?=$os['valor']?>">
+                    </div>
+
+                    <input type="hidden" name="id" value="<?=$id?>">
+
+                    <button class="btn btn-primary float-right" type="submit">Salvar</button>
+                </form>
+            </div>
+        </div>
+    </main>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/js/bootstrap.min.js" integrity="sha384-3qaqj0lc6sV/qpzrc1N5DC6i1VRn/HyX4qdPaiEFbn54VjQBEU341pvjz7Dv3n6P" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- show-os.php
+++ show-os.php
@@ -23,7 +23,7 @@ WHERE o.id=:id;");
 $query->execute([':id' => $id]);
 
 // Recuperando os dados
-$os = $query->fetchAll(PDO::FETCH_ASSOC);
+$os = $query->fetchAll(PDO::FETCH_ASSOC)[0];
 
 // Query equipes
 $queryEquipe = $dbc->prepare("SELECT 
@@ -38,6 +38,33 @@ $queryEquipe->execute([':id'=>$id]);
 
 $equipes = $queryEquipe->fetchAll(PDO::FETCH_ASSOC);
 
+$query = $dbc->prepare("SELECT
+                        o.*,
+                            b.nome,
+                            t.nome
+                        FROM (
+                        oss o
+                            INNER JOIN bairros b ON o.id_bairro=b.id
+                            INNER JOIN tipos_de_os t ON o.id_tipo=t.id
+                        ) WHERE o.id=:id;");
+
+                        //Executando a consulta preparada
+                        $query->execute([
+                            ':id' => $id]);
+
+                        //Capturando o resultado da minha consulta [só a primeira linha]    
+                        $os = $query->fetchAll(PDO::FETCH_ASSOC)[0];
+                        
+
+// Preparar a consulta para levantar os bairros
+$query = $dbc->prepare("SELECT id, nome FROM bairros");
+
+// Executando a consulta
+$query->execute();
+
+// Capturar o resultado da consulta
+$bairros = $query->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -47,27 +74,72 @@ $equipes = $queryEquipe->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+
     <title>Document</title>
 </head>
 
 <body>
-    <?php
-// Mostrando os dados
-    echo('<pre>');
-    print_r($os);
-    echo('</pre>');
 
-    foreach($equipes as $equipe){
-        echo "$equipe[nome] <br>";
-    }
-?>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <!-- Inputs com as informações -->
+                <div class="form-group">
+                    <label for="endereco">Rua</label>
+                    <input type="text" class="form-control" name="" id="endereco" aria-describedby="helpId" value="<?=$os['endereco']?>" readonly>
+                </div>
 
-    <!-- FORMULARIO PARA BOTAO DE EXCLUSAO e EDICAo-->
-    <a href="edit-os.php?id=<?= $id ?>"><button>Editar</button></a>
-    <form action="./includes/delete-os.php" method="post">
-        <input type="hidden" value="<?= $id ?>" name="id_os">
-        <input type="submit" value="Deletar">
-    </form>
+                <div class="form-group">
+                    <label for="bairro">Bairro</label>
+                    <select class="form-control" name="" id="bairro" readonly>
+                        <?php foreach ($bairros as $bairro): ?>
+                            <option <?= $bairro['id'] == $os['id_bairro'] ? 'selected' : '' ?> value="<?=$bairro['id']?>"><?=$bairro['nome']?></option>
+                        <?php endforeach; ?>
+                </div>
+
+                <div class="form-group">
+                    <label for="duracao_prev">Duração Prevista</label>
+                    <input type="text" class="form-control" name="" id="duracao_prev" aria-describedby="helpId" value="<?=$os['duracao_prev']?>" readonly>
+                </div>
+
+                <div class="form-group">
+                    <label for="duracao_real">Duração Real</label>
+                    <input type="text" class="form-control" name="" id="duracao_real" aria-describedby="helpId" value="<?=$os['duracao_real']?>" readonly>
+                </div>
+
+                <div class="form-group">
+                    <label for="valor">Valor</label>
+                    <input type="n" class="form-control" name="" id="valor" aria-describedby="helpId" value="<?=$os['valor']?>" readonly>
+                </div>
+
+                <div class="form-group">
+                    <label for="data">Data</label>
+                    <input type="text" class="form-control" name="" id="data" aria-describedby="helpId" value="<?=$os['data']?>" readonly> 
+                </div>
+
+                <div class="form-group">
+                    <label for="nomeOS">Tipo de OS</label>
+                    <input disable type="text" class="form-control" name="" id="nomeOS" aria-describedby="helpId" value="<?=$os['nome']?>" readonly>
+                </div>
+
+                <div class="form-group">
+                    <label for="equipe">Equipe responsável</label>
+                        <?php foreach($equipes as $equipe): ?>
+                            <input disable type="text" class="form-control" name="" id="equipe" aria-describedby="helpId" value="<?=$equipe['nome']?>" readonly>
+                        <?php endforeach; ?>
+                </div>
+                
+                <!-- FORMULARIO PARA BOTAO DE EXCLUSAO e EDICAO-->
+                
+                <a href="edit-os.php?id=<?= $id ?>"><button class="btn btn-primary float-right" type="submit">Editar</button></a>
+                <form action="./includes/delete-os.php" method="post">
+                    <input type="hidden" value="<?= $id ?>" name="id_os">
+                    <button class="mr-2 btn btn-primary float-right" type="submit">Deletar</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
